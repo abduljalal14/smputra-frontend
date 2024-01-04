76e9a4cef34035e40a60853c410a02c0107f3d1e
abduljalal14 <abdoeljalalexotic@gmail.com>
2024-01-04 11:06:10 +0700

    get order by id and order_id

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 const Api = axios.create({
     //set default endpoint API
-    baseURL: 'https://sarimulyabackend.000webhostapp.com/'
     // baseURL: 'https://sarimulyabackend.000webhostapp.com/'
+    baseURL: 'http://localhost:8000/'
 })
 
 export default Api
